4da239703767cb50eb09b6132b8e2ab5b11c540b
Timofii <timoxafos@gmail.com>
2022-02-09 17:52:21 +0200

    reTask

--- index.js
+++ index.js
@@ -1,92 +1,98 @@
 
-function primeArray(arrayLeng) {
-let result = []
+// function primeArray(arrayLeng) {
+// let result = []
 
-  for (let i = 2; i < 100; i++) {
-    let isPrime = true
-    for (let j = 2; j < i; j++) {
-     if(i%j === 0) {isPrime = false; break}
-         }
-      result.length < arrayLeng && isPrime ? result.push(i) : result
-  }
- return result
-}
-console.log(primeArray(5)) 
-console.log(primeArray(7))
-
-
-//filter numInString
-
-const string = "У Пети было 10 яблок, 2.5 он отдал Маше, 3.5 Васе и 4 оставил себе."
-console.log(string.split(' ').filter(el => !isNaN(Number(el))).map(el => Number(el)))
+//   for (let i = 2; i < 100; i++) {
+//     let isPrime = true
+//     for (let j = 2; j < i; j++) {
+//      if(i%j === 0) {isPrime = false; break}
+//          }
+//       result.length < arrayLeng && isPrime ? result.push(i) : result
+//   }
+//  return result
+// }
+// console.log(primeArray(5)) 
+// console.log(primeArray(7))
 
 
-//count repeat str
+// //filter numInString
 
-clearString = str => {               
-  const valid = str.match(/(.)\1{1,}/g)
-      for(let i in valid) { 
-        str = str.replace(valid[i], valid[i].substr(0,1)+ valid[i].length)
-  }           
-  return str;
-}
-console.log(clearString('assdssddffffrrreeeweggggg'))
+// const string = "У Пети было 10 яблок, 2.5 он отдал Маше, 3.5 Васе и 4 оставил себе."
+// console.log(string.split(' ').filter(el => !isNaN(Number(el))).map(el => Number(el)))
 
 
-//get values in objects
+// //count repeat str
 
-const exampleArr = [{a:{b:[{c:4}, {c:5}]}}, {a:{b:[{c:6},{c:7}]}}]
-const exampleObj = { a:{b:{c:5}}
-}
-let str = ''
-function closure(obj) {
-         for (let iterator in obj) {
-          if(typeof obj[iterator] === 'object' && obj[iterator] !== null) {
-          closure(obj[iterator])
-        } else  { 
-          str+=obj[iterator]
-        } 
-    }
-    return str
-  }
+// clearString = str => {               
+//   const valid = str.match(/(.)\1{1,}/g)
+//       for(let i in valid) { 
+//         str = str.replace(valid[i], valid[i].substr(0,1)+ valid[i].length)
+//   }           
+//   return str;
+// }
+// console.log(clearString('assdssddffffrrreeeweggggg'))
 
-console.log(closure(exampleArr));
+
+//get value in path
+
+// const exampleArr = [{a:{b:[{c:4}, {c:5}]}}, {a:{b:[{c:6},{c:7}]}}]
+// const exampleObj = { a:{b:{c:5}}}
+// let string = '1.a.b.1.c'
+// let string2 = 'a.b'
+
+// function closure(obj, str) {
+//   const arrs = str.split('.')
+//   let currentObj = obj
+//    for (let i = 0; i < str.length; i++) {
+//         let one = arrs.shift() 
+//         if (currentObj[one]) {
+//         currentObj = currentObj[one]
+//      } 
+//    }
+    
+  
+//   return currentObj
+// }
+
+// console.log(closure(exampleObj, string2));
 
 
 // Promises Quque
 
-class ConcurencyQueue {
-  constructor(concurency) {
-    this.lenght = 0;
-    this.queueCurrent = [];
-    this.concurency = concurency;
-  }
+// class ConcurencyQueue {
+//   constructor(concurency) {
+//     this.queueCurrent = [];
+//     this.concurency = concurency;
+//   }
 
-  queue(value) {
-    return  Promise.resolve(() => {
-      return this.queueCurrent.push(value)
-    });
-  }
+//   queue(value) {
+//     return  Promise.resolve(() => {
+//       return this.queueCurrent.push(value)
+//     });
+//   }
    
-   deQueue() {
-    return  Promise.resolve(() => {
-      return this.queueCurrent.shift();
-    });
-  }
+//    deQueue() {
+//     return  Promise.resolve(() => {
+//       return this.queueCurrent.shift();
+//     });
+//   }
 
-  async queuing(req) {
-       while (this.concurency) {
-        await req.then(data => data())
-        this.concurency--
-       }
-  }
-}
+//   async queuing(req) {
+//        while (this.concurency) {
+//         await req.then(data => data())
+//         this.concurency--
+//        }
+//   }
+// }
 
-const concQueue = new ConcurencyQueue(3);
+// const concQueue = new ConcurencyQueue(3);
 
-const req1 = concQueue.queue(5);
-const req2 = concQueue.deQueue();
-concQueue.queuing(req1);
-console.log(concQueue);
+// const req1 = concQueue.queue(5);
+// const req2 = concQueue.deQueue();
+// concQueue.queuing(req1);
+// console.log(concQueue);
+
+
+
 
 
